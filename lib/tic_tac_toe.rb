7bfb931acedebab7bdc8d4e9d46f5b063431a34b
Jakob Baumgartner <jakob.baumgartner@hotmail.com>
2018-09-22 15:17:01 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -44,4 +44,16 @@ class TicTacToe
     end
   end
 
+  def turn_count
+    count = 0
+    @board.each do |element|
+      if element != " "
+        count += 1
+      end
+    end
+    return count
+  end
+
+
+
 end
